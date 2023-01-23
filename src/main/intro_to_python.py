11c6144f976c95b1a0f984dfbde91f5887399f2e
Noah-G2 <122494073+Noah-G2@users.noreply.github.com>
2023-01-23 16:01:02 +0000

    code input

---
+++ src/main/intro_to_python.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+m1 = np.matrix ([[1,0,0],[0,1,0],[0,0,1]])
+print (m1)
+
+
+m2 = np.matrix ([[1,3,3],[3,1,3],[3,3,1]])
+print(m2)
+
+m3 = np.matrix ([[1,3],[3,1],[3,3]])
+print (m3)
